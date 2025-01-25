9b6a3de66ed6da076477902f2b5bcc12a783785d
Grandced <125399406+Grandced@users.noreply.github.com>
2025-01-25 10:16:51 +0100

    photo

--- header.php
+++ header.php
@@ -7,9 +7,28 @@
 </head>
 <body <?php body_class(); ?>>
 <header>
-    <h1><?php bloginfo( 'name' ); ?></h1>
-    <p><?php bloginfo( 'description' ); ?></p>
-    <nav>
-        <?php wp_nav_menu( array( 'theme_location' => 'primary-menu' ) ); ?>
-    </nav>
+<!-- Menu header--> 
+    <div class="header-container">
+        <!-- Logo -->
+        <a href="<?php echo home_url(); ?>" class="site-logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" alt="Logo">
+        </a>
+        <!-- Menu de navigation -->
+        <nav class="site-nav">
+        <nav class="site-nav">
+        <?php
+        wp_nav_menu(array(
+            'theme_location' => 'header-menu', // Assure-toi que l'emplacement est correct
+            'menu_class' => 'nav-list',
+            'container' => false, // Supprime le conteneur par défaut
+        ));
+        ?>
+        </nav>
+    </div> 
+    
+<!-- image du header -->
+    <?php
+    $image_url = get_template_directory_uri() . '/assets/images/header.png';
+    ?>
+    <img src="<?php echo esc_url($image_url); ?>" alt="Header Image" class="header-image">
 </header>
